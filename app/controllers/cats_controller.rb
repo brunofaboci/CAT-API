0fe55bfaeffe067c1101fe1b6c2e8ed364a7f022
brunofaboci <brunofaboci@gmail.com>
2023-10-23 16:36:09 -0300

    some adjusts

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,6 +3,6 @@ class CatsController < ApplicationController
   def index
     cats = Cat.pagy_search(params[:query])
 
-    @pagy, @cats = pagy_meilisearch(cats, items: 10)
+    @pagy, @cats = pagy_meilisearch(cats, items: 5)
   end
 end
